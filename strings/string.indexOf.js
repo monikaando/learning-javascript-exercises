e7423fc4b6bb3efec3075d54bd6940d5147ab4f5
Monika <mswidzinska87@gmail.com>
2019-08-01 13:47:55 +0200

    learning

---
+++ strings/string.indexOf.js
@@ -0,0 +1,7 @@
+var phrase = 'czy to kot, czy coś innego ? ';
+var index = phrase.indexOf('kot');
+console.log('Słowo kot zapisano, zaczynając od indeksu ' + index);
+index = phrase.indexOf('czy', 7);
+console.log('Słowo czy zapisano, zaczynając od indeksu ' + index);
+index = phrase.indexOf('pies');
+console.log('Słowo pies zapisano, zaczynając od indeksu ' + index);
