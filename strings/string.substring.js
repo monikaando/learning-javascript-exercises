c8a14af77fad42d74dab87d090e6866c6b01f09b
Monika <mswidzinska87@gmail.com>
2019-08-01 13:53:14 +0200

    Update string.substring.js

---
+++ strings/string.substring.js
@@ -0,0 +1,6 @@
+var data = 'name | phone | adress';
+var val = data.substring(5, 12);
+console.log('Selected fragment: : ' + val);
+
+val = data.substring(5);
+console.log('The next fragment ' + val);
