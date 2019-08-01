7aa9416446a1da7090426d67e037309bc2ea85e3
Monika <mswidzinska87@gmail.com>
2019-08-01 13:37:31 +0200

    learning

---
+++ strings/string.length.js
@@ -0,0 +1,6 @@
+var input = 'janka@bardzosprytni.com.pl';
+for (var i = 0; i < input.length; i++) {
+    if (input.charAt(i) === '@') {
+        console.log('A sign @ has index ' + i);
+    }
+}
---
+++ strings/string_object.js
@@ -0,0 +1,8 @@
+var name = 'Anna';
+var phone = '867-5309';
+var fact = 'To liczba pierwsza';
+var songName = phone + '/' + name;
+var index = phone.indexOf('-');
+if (fact.substring(10, 18) === 'pierwsza') {
+    alert(fact);
+}
