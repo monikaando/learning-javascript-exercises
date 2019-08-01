c30227a6e072eb44dea39f559df1d794bf14a893
Monika <mswidzinska87@gmail.com>
2019-08-01 13:49:27 +0200

    comment

--- strings/string.indexOf.js
+++ strings/string.indexOf.js
@@ -2,6 +2,6 @@ var phrase = 'czy to kot, czy coś innego ? ';
 var index = phrase.indexOf('kot');
 console.log('Słowo kot zapisano, zaczynając od indeksu ' + index);
 index = phrase.indexOf('czy', 7);
-console.log('Słowo czy zapisano, zaczynając od indeksu ' + index);
+console.log('Słowo czy zapisano, zaczynając od indeksu ' + index); /*it skips the first czy*/
 index = phrase.indexOf('pies');
 console.log('Słowo pies zapisano, zaczynając od indeksu ' + index);
